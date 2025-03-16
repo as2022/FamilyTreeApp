e86bc713a2724f2ad2c6a8ab15833cc9e4435c77
as2022 <77690014+as2022@users.noreply.github.com>
2025-03-15 21:59:33 -0400

    Initial Commit

---
+++ BinaryTree/BinaryTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinaryTreeApp.swift
+//  BinaryTree
+//
+//  Created by Alex Smithson on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct BinaryTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
